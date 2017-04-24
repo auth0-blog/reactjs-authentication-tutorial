1e26ce8cff3474c931eaf5355138f1d98b4febc0
PROSPER OTEMUYIWA <prosperotemuyiwa@gmail.com>
2017-04-24 22:09:04 +0100

    Add jwks

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
