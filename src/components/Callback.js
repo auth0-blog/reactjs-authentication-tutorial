91f4fb47615cca36eeefd5fd821354070b109e40
Drew Fyock <fyockm@gmail.com>
2017-04-25 23:22:36 -0400

    Useless constructor in `Callback`
    ```
    /reactjs-authentication-tutorial/src/components/Callback.js
      6:3  warning  Useless constructor  no-useless-constructor
    ```

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -3,10 +3,6 @@ import { setIdToken, setAccessToken } from '../utils/AuthService';
 
 class Callback extends Component {
 
-  constructor() {
-    super()
-  }
-
   componentDidMount() {
     setAccessToken();
     setIdToken();
